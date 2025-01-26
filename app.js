fe4aecad62efed0598547e3ebc1493a816683805
Max Reis <maxreis507@gmail.com>
2025-01-26 18:14:05 +0100

    updata package.json and azure pipeline

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}`)
+    console.log(`App running on port ${port}`);
 })
